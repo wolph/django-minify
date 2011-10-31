134481ed81bf54ea3e8250610da495353ce881c6
WoLpH <Rick@Fawo.nl>
2011-10-31 15:56:26 +0100

    bumping version for the dummy write patch

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ else:
 
 setup(
     name = 'django-minify',
-    version = '1.3',
+    version = '1.3.1',
     author = 'Rick van Hattem',
     author_email = 'Rick.van.Hattem@Fawo.nl',
     description = '''django-minify is a django app that combines and minifies
